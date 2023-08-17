7fdb481620dc0d485c70351a495ba8337812f4a3
trevor dewindt <trevor.dewindt@gmail.com>
2023-08-16 19:30:52 -0600

    add code

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import ipdb
 
 APPROVED_BREEDS = [
     "Mastiff",
@@ -12,4 +13,35 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name = '', breed = ''):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        # ipdb.set_trace()
+        if ((type(name) == str) and (1 <= len(name) <= 25)):
+            self._name = name
+        else:
+            print('Name must be string between 1 and 25 characters.')
+
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        # ipdb.set_trace()
+        # if not hasattr(self, 'name'):
+        #     return
+
+        if (breed in APPROVED_BREEDS):
+            self._breed = breed
+        else:
+            print('Breed must be in list of approved breeds.')
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+
+
+d = Dog(name = "")
